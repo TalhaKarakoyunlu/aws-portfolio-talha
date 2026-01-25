63d684bf0fda8689c4ecafd350cdfa0626056dcc
TALHA BAHADIR KARAKOYUNLU <karakoyunlutalha34@gmail.com>
2026-01-26 02:37:25 +0300

    refactor: update homepage content to test automation

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          bindik bi alamete
+          Automated OIDC deployment workflow test
         </p>
       </div>
       <p className="read-the-docs">
